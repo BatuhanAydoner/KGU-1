eeb732dd3cf8c5969a4d94e7d22784e3962a5084
Kadir Ince <you.can.send@pm.me>
2021-03-16 09:28:54 +0300

    Create Home Page Template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app >
+    <v-container class="home-body">
+      <nav class="container d-flex justify-content-between">
+        <img class="logo" src="@/assets/Logo.png" alt="Logo" />
+        <div>
+          <router-link to="/">Ana Sayfa</router-link> |
+          <router-link to="about">Nasıl Kullanılır?</router-link> |
+          <router-link to="about">Giriş Yap</router-link>
+        </div>
+      </nav>
+      <div class="desription">
+        <h1 class="">Header</h1>
+        <p class="text-secondary">aciklama</p>
+      </div>
+    </v-container>
+    <img
+      class="img-purple-hand"
+      src="@/assets/without_login/purple_hand.png"
+      alt="purple hand"
+    />
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style>
+.home-body {
+  font-family: "Comfortaa", cursive;
+}
+.img-purple-hand {
+  height: 80vh;
+  position: absolute;
+  bottom: 0;
+  right: 7vw;
+}
+.desription{
+  margin-top: 9vh;
+}
+
+.logo {
+  width: 7vw;
+}
+</style>
